8e8842baeef050a1d324eca7392d530b13dff7ed
Chai <isaigomezhernandez@gmail.com>
2020-01-31 22:45:49 -0600

    react-bootstrap en Pokemon

--- src/component/pokemon/index.js
+++ src/component/pokemon/index.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import { Row, Card, Col,Container } from 'react-bootstrap';
 import PokemonInfo from '../pokemonInfo';
 import Name from './Name';
 import pokemon from '../../img/pokemon.jpg'
 import './styles.css'
+
 
 const obPokemon = {
     name:'Pikachu',
@@ -10,20 +12,47 @@ const obPokemon = {
     number:25
 }
 
-class Pokemon extends Component{     
+class Pokemon extends Component {     
     render(){
         const { type, number } = obPokemon;
         return(
-            <div className="pokemonCont">
-                <Name name={obPokemon.name}></Name>
-                <div className="image-info">  
-                <img src = {pokemon} height="100" width="100"/>
-                <PokemonInfo type={type} number={number}></PokemonInfo>           
-                </div>
-
-            </div>
-        )
+            <Container> 
+                <Row>
+                    <Col>
+                        <Card bg="secondary" text="white">
+                            <Card.Header>POKEMON</Card.Header>
+                        </Card>                    
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={12} md={4}>
+                        <Card style={{width: "20em"}}>
+                                <Card.Img variant="top" src={pokemon}/>
+                            </Card>               
+                        </Col>
+                        <Col xs={12} md={8}>
+                            <Card bg="ligth">
+                                <Card.Header>
+                                    <Name name={obPokemon.name}></Name>
+                                </Card.Header>
+                                <Card.Body>
+                                    <Card.Text>
+                                        <PokemonInfo
+                                        type={type}
+                                        number={number}>
+                                        </PokemonInfo> 
+                                    </Card.Text>
+                                </Card.Body>          
+                            </Card>          
+                        </Col>
+                    </Row>
+                </Container>)
+        }
     }
-}
 
+                            
 export default Pokemon;
+                
+                
+
+
