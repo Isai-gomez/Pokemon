bea82129be1053129759c4761468476f73ed99c2
Chai <isaigomezhernandez@gmail.com>
2020-02-01 00:10:30 -0600

    Funciona la peticion a la API

--- src/component/pokemon/index.js
+++ src/component/pokemon/index.js
@@ -1,20 +1,43 @@
 import React, { Component } from 'react';
-import { Row, Card, Col,Container } from 'react-bootstrap';
+import { Row, Card, Col, Container } from 'react-bootstrap';
 import PokemonInfo from '../pokemonInfo';
 import Name from './Name';
-import pokemon from '../../img/pokemon.jpg'
-import './styles.css'
+import pokemon from '../../img/pokemon.jpg';
 
 
-const obPokemon = {
-    name:'Pikachu',
-    type:'Elecctrico',
-    number:25
-}
-
-class Pokemon extends Component {     
+class Pokemon extends Component {
+    constructor(){
+        super();
+        this.state = {
+            name:"Pikachu",
+            type:"Electrico",
+            number: 25,
+            photo:pokemon,
+        }
+    }
+    componentDidMount(){
+        this.handleUpdate();
+    }
+    getUrlPokemo = number => {
+        const api_pokemon = `https://pokeapi.co/api/v2/pokemon/${number}`;
+        return (api_pokemon);
+    }
+    handleUpdate = () => {
+        const api_pokemon = this.getUrlPokemo(34);
+        fetch(api_pokemon)
+            .then( resolve =>{
+                return resolve.json()
+            })
+            .then(data => {
+               this.setState({
+                   name:data.species.name.toUpperCase(),
+                   photo:data.sprites.front_shiny,
+               })
+            });
+                
+    }     
     render(){
-        const { type, number } = obPokemon;
+        const { name, type, number, photo } = this.state;
         return(
             <Container> 
                 <Row>
@@ -27,28 +50,29 @@ class Pokemon extends Component {
                 <Row>
                     <Col xs={12} md={4}>
                         <Card style={{width: "20em"}}>
-                                <Card.Img variant="top" src={pokemon}/>
+                                <Card.Img variant="top" src={photo}/>
                             </Card>               
-                        </Col>
-                        <Col xs={12} md={8}>
-                            <Card bg="ligth">
-                                <Card.Header>
-                                    <Name name={obPokemon.name}></Name>
-                                </Card.Header>
-                                <Card.Body>
-                                    <Card.Text>
-                                        <PokemonInfo
-                                        type={type}
-                                        number={number}>
-                                        </PokemonInfo> 
-                                    </Card.Text>
-                                </Card.Body>          
-                            </Card>          
-                        </Col>
-                    </Row>
-                </Container>)
-        }
+                    </Col>
+                    <Col xs={12} md={8}>
+                        <Card bg="ligth">
+                            <Card.Header>
+                                <Name name={ name }></Name>
+                            </Card.Header>
+                            <Card.Body>
+                                <Card.Text>
+                                    <PokemonInfo
+                                    type={ type }
+                                    number={ number }>
+                                    </PokemonInfo> 
+                                </Card.Text>
+                            </Card.Body>          
+                        </Card>          
+                    </Col>
+                </Row>
+            </Container>
+        )
     }
+}
 
                             
 export default Pokemon;
