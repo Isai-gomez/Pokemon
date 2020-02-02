975496837a64682f749de7c6884bf99bda645508
Chai <isaigomezhernandez@gmail.com>
2020-02-02 15:31:14 -0600

    Separe constantes y servicios de la vista

--- src/component/pokemon/index.js
+++ src/component/pokemon/index.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 import Forms from './Forms';
-import { Row, Card, Col, Container, Spinner } from 'react-bootstrap';
-import PokemonInfo from './pokemonInfo';
-import Name from './Name';
-
-
+import { getUrlPokemon } from '../service/getUrlPokemon';
+import knowValue  from '../service/knowValue'
+import { Row, Card, Col, Container } from 'react-bootstrap';
 
 class Pokemon extends Component {
     constructor(){
@@ -14,24 +12,18 @@ class Pokemon extends Component {
 
         }
     }
-   
-    
     componentDidMount(){
        this.handleUpdate();
     }
-    getUrlPokemo = ( number = 25 ) => {
-        const api_pokemon = `https://pokeapi.co/api/v2/pokemon/${number}`;
-        return (api_pokemon);
-    }
+    
     handleUpdate = ( number )=> {
-        const api_pokemon = this.getUrlPokemo(number);
+        const api_pokemon = getUrlPokemon(number);
         fetch(api_pokemon)
             .then( resolve =>{
                 return resolve.json()
             })
             .then(data => {
-               
-               this.setState({
+                this.setState({
                    data:{
                    name:data.species.name.toUpperCase(),
                    photo:data.sprites.front_shiny,
@@ -39,44 +31,17 @@ class Pokemon extends Component {
                    type:data.types[0].type.name.toUpperCase(),
                    }
                })
-            });
-                
-    }     
+            });                
+    }
+
     render(){
-        const { data } = this.state;
-        const knowValue = ( data ) => {
-            return(
-                (data) ? 
-                <Row>
-                <Col md={5}>
-                    <Card style={{width: "15em"}}>
-                        <Card.Img variant="top" src={data.photo}/>
-                     </Card>               
-                </Col> 
-                <Col xs={12} md={7}>
-                    <Card bg="ligth">
-                        <Card.Header>
-                            <Name name={ data.name }></Name>
-                        </Card.Header>
-                        <Card.Body>
-                            <Card.Text>
-                                <PokemonInfo
-                                    type={ data.type }
-                                    number={ data.number }>
-                                </PokemonInfo> 
-                            </Card.Text>
-                        </Card.Body>          
-                    </Card>
-                </Col>
-            </Row>:
-            <Spinner animation="grow"/>);
-        }
+        const { data } = this.state;        
         return(
             <Container>
                 <Row style={{marginTop:"3em"}}>
                     <Col>
                         <Card bg="secondary" text="white">
-                            <Card.Header>POKEMON</Card.Header>
+                            <Card.Header>BUSCADOR DE POKEMON</Card.Header>
                         </Card>                    
                     </Col>
                 </Row>
@@ -85,7 +50,7 @@ class Pokemon extends Component {
                         <Forms handleUpdate={this.handleUpdate}></Forms>
                     </Col>
                     <Col> 
-                    {knowValue(data)}
+                    { knowValue(data) }
                     </Col>
                 </Row>
             </Container>
